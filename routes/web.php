73c7517a59cafd85b14dad17cc2c168f02c452f7
fatima31312 <10121473@mu.edu.lb>
2025-01-03 11:55:20 +0200

    add at web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelocomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelocomeController::class, 'welcome'])->name('welcome');
